5e4cff3fe9a266b2f5282c1a0a8d90e5930f9d2d
Evgeniy Malashenko <111k1808@gmai.com>
2019-11-03 18:59:22 +0200

    Добавляет асерты на все варианты переходов

--- public/test.php
+++ public/test.php
@@ -1,11 +1,12 @@
 <?php
+use app\classes\Task;
 
-function my_autoloader($class) {
-    include 'classes/' . $class . '.php';
-}
+require_once __DIR__ . '/../vendor/autoload.php';
 
-spl_autoload_register('my_autoloader');
-
-$task = new Task;
-echo assert($task->getNextStatus(Task::ACTION_CREATE_NEW) === Task::STATUS_NEW, 'при создании новой задачи возвращается статус новая');
-
+$task = new Task(26, '18/012/2019');
+assert($task->getNextStatus(Task::ACTION_CREATE_NEW) === Task::STATUS_NEW, 'при создании новой задачи возвращается статус новая');
+assert($task->getNextStatus(Task::ACTION_CANCEL) === Task::STATUS_CANCELED, 'при создании новой задачи возвращается статус новая');
+assert($task->getNextStatus(Task::ACTION_RESPOND) === Task::STATUS_IN_WORK, 'при создании новой задачи возвращается статус новая');
+assert($task->getNextStatus(Task::ACTION_DONE) === Task::STATUS_COMPLETED, 'при создании новой задачи возвращается статус новая');
+assert($task->getNextStatus(Task::ACTION_REFUSE) === Task::STATUS_FAILED, 'при создании новой задачи возвращается статус новая');
+echo 'Все тесты пройдены!';
---
+++ src/classes/Task.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace app\classes;
+class Task
+{
+
+    //блок констант статусов
+    const STATUS_NEW = 'Новое';
+    const STATUS_CANCELED = 'Отменено';
+    const STATUS_IN_WORK = 'В работе';
+    const STATUS_COMPLETED = 'Выполнено';
+    const STATUS_FAILED = 'Провалено';
+    //блок констант действий
+    const ACTION_CREATE_NEW = 'создание новой';
+    const ACTION_CANCEL = 'отменить';
+    const ACTION_RESPOND = 'Откликнуться';
+    const ACTION_DONE = 'Выполнено';
+    const ACTION_REFUSE = 'Отказаться';
+
+    //блок свойств
+    private $executorId;
+    private $customerId;
+    private $completionTime;
+    private $status;
+
+    public function __construct(int $customerId, string $completionTime)
+    {
+        $this->customerId = $customerId;
+        $this->completionTime = $completionTime;
+        $this->status = self::STATUS_NEW;
+    }
+
+    //блок методов
+    public function getNextStatus($action)
+    {
+        switch ($action) {
+            case self::ACTION_CREATE_NEW:
+                return self::STATUS_NEW;
+                break;
+            case self::ACTION_CANCEL:
+                return self::STATUS_CANCELED;
+                break;
+            case self::ACTION_RESPOND:
+                return self::STATUS_IN_WORK;
+                break;
+            case self::ACTION_DONE:
+                return self::STATUS_COMPLETED;
+                break;
+            case self::ACTION_REFUSE:
+                return self::STATUS_FAILED;
+                break;
+        }
+        return null;
+    }
+}
