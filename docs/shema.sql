ca7a8fbcdbb4d65d4ff4377a231bdee55ee439b1
Evgeniy Malashenko <111k1808@gmai.com>
2019-11-07 21:57:54 +0200

    Добавляет NULL необязательным полям

--- docs/shema.sql
+++ docs/shema.sql
@@ -14,27 +14,27 @@ CREATE DATABASE taskforce
     `creation_time` DATETIME DEFAULT NOW() NOT NULL,
     `name_user` VARCHAR(128) NOT NULL,
     `email` VARCHAR(128) NOT NULL,
-    `location` VARCHAR(128),
+    `location` VARCHAR(128) NULL,
     `birthday` DATETIME NOT NULL,  
-    `contact_details` TEXT,
-    `category_id` INT,
+    `contact_details` TEXT NULL,
+    `category_id` INT NULL,
     `password` VARCHAR(128) NOT NULL,
-    `phone` VARCHAR(128) ,
-    `skype` VARCHAR(128) ,
+    `phone` VARCHAR(128) NULL NULL,
+    `skype` VARCHAR(128) NULL NULL,
     `another_messenger` VARCHAR(128) , 
     `photos_works` VARCHAR(128),    
     `avatar`  VARCHAR(128),
-    `task_id` INT ,
+    `task_id` INT NULL NULL,
     `status_task` VARCHAR(128)  
   );
   
   CREATE TABLE reviews (
     `id` INT AUTO_INCREMENT PRIMARY KEY,
-    `user_id` INT,
-    `reviewer_id` INT,
-    `assessment` INT,
-    `task_id` INT,
-    `review_text` TEXT
+    `user_id` INT NULL,
+    `reviewer_id` INT NULL,
+    `assessment` INT NULL,
+    `task_id` INT NULL,
+    `review_text` TEXT NULL
   );
 
   CREATE TABLE task (
@@ -42,14 +42,14 @@ CREATE DATABASE taskforce
     `creation_time` DATETIME DEFAULT NOW() NOT NULL,
     `name_task` VARCHAR(128) NOT NULL,
     `category_id` INT NOT NULL,
-    `description` TEXT,
-    `location` INT,
+    `description` TEXT NULL,
+    `location` INT NULL,
     `budget` INT NOT NULL,
     `deadline` DATETIME NOT NULL,
-    `file`  VARCHAR(128),
+    `file`  VARCHAR(128) NULL,
     `author_id` INT NOT NULL,
-    `executor_id` INT,
-    correspondence_id INT
+    `executor_id` INT NULL,
+    correspondence_id INT NULL
   );
   
   CREATE TABLE correspondence (
