36a29461c9ba23bfbcb9a3f45fa2c42419dc5247
malashenkoEvgeniy <111k1808@gmail.com>
2019-11-19 20:33:12 +0200

    дорабатка схемы БД

--- docs/shema.sql
+++ docs/shema.sql
@@ -6,14 +6,14 @@ CREATE DATABASE taskforce
 
   CREATE TABLE categories (
     `id` INT AUTO_INCREMENT PRIMARY KEY,
-    `user_id` INT NOT NULL,
     `title` VARCHAR(128) NOT NULL
   );
   
-  CREATE TABLE response_task (
+  CREATE TABLE proposal (
     `id` INT AUTO_INCREMENT PRIMARY KEY,
-    `response` VARCHAR(128) NOT NULL,
+    `comment` VARCHAR(128) NOT NULL,
     `task_id` INT NOT NULL,
+    `budget` INT NULL,
     `user_id` INT NOT NULL
   );
   
@@ -25,13 +25,15 @@ CREATE DATABASE taskforce
   `email` VARCHAR(128) NOT NULL,
   `location_id` INT NULL,
   `birthday` DATETIME NULL,  
-  `contact_details` TEXT NULL,
+  `info` TEXT NULL,
   `password` VARCHAR(128) NOT NULL,
   `phone` VARCHAR(128) NULL,
   `skype` VARCHAR(128) NULL,
   `another_messenger` VARCHAR(128) NULL, 
   `avatar`  VARCHAR(128) NULL,
-  `task_name` VARCHAR(128) NULL
+  `task_name` VARCHAR(128) NULL,
+  `show_contacts_for_customer`  TINYINT NOT NULL DEFAULT 0,
+  `hide_profile`  TINYINT NOT NULL DEFAULT 0  
   );
   
   CREATE TABLE users_categories (
@@ -43,20 +45,20 @@ CREATE DATABASE taskforce
   CREATE TABLE email_settings (
   `id` INT AUTO_INCREMENT PRIMARY KEY,
   `user_id` INT NOT NULL,  
-  `new_response` TINYINT NOT NULL,
-  `new_chat_message` TINYINT NOT NULL,
-  `task_abandonment` TINYINT NOT NULL,
+  `proposal` TINYINT NOT NULL,
+  `chat_message` TINYINT NOT NULL,
+  `refuse` TINYINT NOT NULL,
   `start_task` TINYINT NOT NULL,
-  `task_completion` TINYINT NOT NULL
+  `completion_task` TINYINT NOT NULL
   );
   
   CREATE TABLE reviews (
     `id` INT AUTO_INCREMENT PRIMARY KEY,
-    `user_id` INT NULL,
-    `reviewer_id` INT NULL,
+    `executor_id` INT NULL,
+    `customer_id` INT NULL,
     `assessment` INT NULL,
     `task_id` INT NULL,
-    `review_text` TEXT NULL
+    `comment` TEXT NULL
   );
 
   CREATE TABLE task (
@@ -68,22 +70,21 @@ CREATE DATABASE taskforce
     `location_id` INT NULL,
     `budget` INT NULL,
     `deadline` DATETIME NOT NULL,
-    `author_id` INT NOT NULL,
+    `customer_id` INT NOT NULL,
     `executor_id` INT NULL,
-    `correspondence`VARCHAR(128) NULL,
     `status` VARCHAR(128) NULL  
   );
   
-  CREATE TABLE correspondence (
+  CREATE TABLE chat_messages (
     `id` INT AUTO_INCREMENT PRIMARY KEY,
     `task_id` INT NOT NULL,
     `writer_id` INT NOT NULL,
-    `text` TEXT NOT NULL,
+    `comment` TEXT NOT NULL,
     `creation_time` DATETIME DEFAULT NOW() NOT NULL,
     `viewed` TINYINT NOT NULL 
   );
   
-    CREATE TABLE locations (
+  CREATE TABLE locations (
     `id` INT AUTO_INCREMENT PRIMARY KEY,
     `city` VARCHAR(128) NOT NULL
   );
