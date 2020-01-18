e7bdba63e44f4ddc4fc4c7e2ae1821b986272b95
malashenkoEvgeniy <111k1808@gmail.com>
2020-01-18 12:43:23 +0200

    Исправляет разделители

--- public/createdb.php
+++ public/createdb.php
@@ -9,27 +9,27 @@ use app\classes\sqlFileCreater\TasksSqlFileCreater;
 use app\classes\sqlFileCreater\ReviewsSqlFileCreater;
 
 require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__.'\..\function.php';
+require_once __DIR__.'/../function.php';
 
 $category = new CategorySqlFileCreater(__DIR__ . '/data/categories.csv');
 $category->execute();
 
-$profiles = new ProfilesSqlFileCreater(__DIR__ . '\data\profiles.csv');
+$profiles = new ProfilesSqlFileCreater(__DIR__ . '/data/profiles.csv');
 $profilesArr = $profiles->execute();
 
-$cities = new CitiesSqlFileCreater(__DIR__ . '\data\cities.csv');
+$cities = new CitiesSqlFileCreater(__DIR__ . '/data/cities.csv');
 $cityArr = $cities->execute();
 
 
-$users = new UsersSqlFileCreater(__DIR__ . '\data\users.csv');
+$users = new UsersSqlFileCreater(__DIR__ . '/data/users.csv');
 $users->execute();
 $user = $users->createUsersFile($profilesArr, $cityArr);
 
-$tasks = new TasksSqlFileCreater(__DIR__ . '\data\tasks.csv');
+$tasks = new TasksSqlFileCreater(__DIR__ . '/data/tasks.csv');
 $arrTask = $tasks->execute();
 $tasks->createTasksFile($users->cities, count($user));
 
 
-$reviews = new ReviewsSqlFileCreater(__DIR__ . '\data\opinions.csv');
+$reviews = new ReviewsSqlFileCreater(__DIR__ . '/data/opinions.csv');
 $reviews->execute();
 $reviews->createReviewsFile(count($user), count($arrTask));
