0ea65f75436cee1031a319f9118b29a219be5695
malashenkoEvgeniy <111k1808@gmail.com>
2020-02-02 21:38:25 +0200

    Добавляет класс UsersCategories

---
+++ frontend/models/UsersCategories.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "users_categories".
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property int $category_id
+ */
+class UsersCategories extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'users_categories';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['user_id', 'category_id'], 'required'],
+            [['user_id', 'category_id'], 'integer'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'user_id' => 'User ID',
+            'category_id' => 'Category ID',
+        ];
+    }
+}
