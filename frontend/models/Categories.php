aa796aedf948684d39eb74241d1a0382738d643f
malashenkoEvgeniy <111k1808@gmail.com>
2020-02-02 21:32:29 +0200

    Добавляет класс Categories

---
+++ frontend/models/Categories.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "categories".
+ *
+ * @property int $id
+ * @property string $title
+ * @property string $title_en
+ */
+class Categories extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'categories';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['title', 'title_en'], 'required'],
+            [['title', 'title_en'], 'string', 'max' => 128],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+            'title_en' => 'Title En',
+        ];
+    }
+}
